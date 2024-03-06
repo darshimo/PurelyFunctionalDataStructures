ac8fe27c8c6c84038eab38caf858f4986fa9f1b3
darshimo <takumishimoda7623@gmail.com>
2024-03-06 17:36:41 +0900

    CustomStackにsuffixesを実装

--- rust/src/chap02/custom_stack.rs
+++ rust/src/chap02/custom_stack.rs
@@ -1,6 +1,6 @@
 // 図2.3
 
-use std::rc::Rc;
+use std::{fmt::Debug, rc::Rc};
 
 use super::super::common::stack::Stack;
 
@@ -10,6 +10,21 @@ enum CustomStackCell<T> {
     Cons(T, CustomStack<T>),
 }
 use CustomStackCell::*;
+
+impl<T> Clone for CustomStack<T> {
+    fn clone(&self) -> Self {
+        CustomStack(self.0.clone())
+    }
+}
+
+impl<T: Debug> Debug for CustomStack<T> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match &*self.0 {
+            Nil => write!(f, "[]"),
+            Cons(x, t) => write!(f, "({:?}) :: {:?}", x, t),
+        }
+    }
+}
 
 impl<T: Clone> Stack<T> for CustomStack<T> {
     fn empty() -> Self {
@@ -40,5 +55,26 @@ impl<T: Clone> Stack<T> for CustomStack<T> {
             Cons(_, t) => CustomStack(t.0.clone()),
             Nil => panic!("empty stream."),
         }
+    }
+}
+
+impl<T: Clone> CustomStack<T> {
+    // 演習問題 2.1
+    fn suffixes(&self) -> CustomStack<CustomStack<T>> {
+        match &*self.0 {
+            Nil => CustomStack::empty().cons(self.clone()),
+            Cons(_, t) => t.suffixes().cons(self.clone()),
+        }
+    }
+}
+
+mod test {
+    use super::CustomStack;
+    use crate::common::stack::Stack;
+
+    #[test]
+    fn test_suffixes() {
+        let l = CustomStack::empty().cons(4).cons(3).cons(2).cons(1u32);
+        println!("{:?}", l.suffixes());
     }
 }
