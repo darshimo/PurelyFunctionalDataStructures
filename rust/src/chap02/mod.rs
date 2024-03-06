75467935fc0a3e4bec0c66c505fee9c164b4baf2
darshimo <takumishimoda7623@gmail.com>
2024-03-06 16:38:28 +0900

    finite_map,unbalanced_mapを追加

--- rust/src/chap02/mod.rs
+++ rust/src/chap02/mod.rs
@@ -1,5 +1,7 @@
 mod custom_stack;
+mod finite_map;
 mod ordered;
 mod set;
 mod stack;
+mod unbalanced_map;
 mod unbalanced_set;
---
+++ rust/src/chap02/unbalanced_map.rs
@@ -0,0 +1,70 @@
+// 演習問題 2.6
+
+use std::rc::Rc;
+
+use super::{finite_map::FiniteMap, ordered::Ordered};
+
+struct UnbalancedMap<K, V>(Rc<UnbalancedMapCell<K, V>>);
+
+enum UnbalancedMapCell<K, V> {
+    E,
+    T(UnbalancedMap<K, V>, K, V, UnbalancedMap<K, V>),
+}
+use UnbalancedMapCell::*;
+
+impl<K, V> Clone for UnbalancedMap<K, V> {
+    fn clone(&self) -> Self {
+        UnbalancedMap(self.0.clone())
+    }
+}
+
+impl<K: Ordered + Clone, V: Clone> FiniteMap<K, V> for UnbalancedMap<K, V> {
+    fn empty() -> Self {
+        UnbalancedMap(Rc::new(E))
+    }
+
+    fn bind(&self, key: K, value: V) -> Self {
+        match &*self.0 {
+            E => UnbalancedMap(Rc::new(T(
+                UnbalancedMap::empty(),
+                key,
+                value,
+                UnbalancedMap::empty(),
+            ))),
+            T(a, key_, value_, b) => {
+                if key.lt(key_) {
+                    UnbalancedMap(Rc::new(T(
+                        a.bind(key, value),
+                        key_.clone(),
+                        value_.clone(),
+                        b.clone(),
+                    )))
+                } else if key_.lt(&key) {
+                    UnbalancedMap(Rc::new(T(
+                        a.clone(),
+                        key_.clone(),
+                        value_.clone(),
+                        b.bind(key, value),
+                    )))
+                } else {
+                    UnbalancedMap(Rc::new(T(a.clone(), key, value, b.clone())))
+                }
+            }
+        }
+    }
+
+    fn lookup(&self, key: K) -> V {
+        match &*self.0 {
+            E => panic!("not found."),
+            T(a, key_, value_, b) => {
+                if key.lt(key_) {
+                    a.lookup(key)
+                } else if key_.lt(&key) {
+                    b.lookup(key)
+                } else {
+                    value_.clone()
+                }
+            }
+        }
+    }
+}
