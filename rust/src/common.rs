21b2b9114cfdc29abea8cdbea21887e1239d894b
darshimo <takumishimoda7623@gmail.com>
2024-03-17 13:47:28 +0900

    trait Queueを追加

--- rust/src/common.rs
+++ rust/src/common.rs
@@ -1,6 +1,7 @@
 pub mod finite_map;
 pub mod heap;
 pub mod ordered;
+pub mod queue;
 pub mod set;
 pub mod stack;
 pub mod stream;
---
+++ rust/src/common/queue.rs
@@ -0,0 +1,8 @@
+pub trait Queue<T> {
+    fn empty() -> Self;
+    fn is_empty(&self) -> bool;
+
+    fn snoc(&self, x: T) -> Self;
+    fn head(&self) -> T;
+    fn tail(&self) -> Self;
+}
